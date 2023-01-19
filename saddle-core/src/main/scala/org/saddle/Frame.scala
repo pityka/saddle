f8c29e27c1c7a11be82fa7d82900e68b91a1f587
Scala Steward <scala_steward@virtuslab.com>
2023-01-19 21:06:15 +0000

    Reformat with scalafmt 3.7.0
    Executed command: scalafmt --non-interactive

--- saddle-core/src/main/scala/org/saddle/Frame.scala
+++ saddle-core/src/main/scala/org/saddle/Frame.scala
@@ -945,7 +945,8 @@ class Frame[RX: ST: ORD, CX: ST: ORD, @spec(Int, Long, Double) T](
     * @param limit
     *   If > 0, propagate over a maximum of `limit` consecutive NA values.
     */
-  def fillNA(fillMethod: FillMethod, limit: Int = 0) = mapCols((_, c) => c.fillNA(fillMethod, limit))
+  def fillNA(fillMethod: FillMethod, limit: Int = 0) =
+    mapCols((_, c) => c.fillNA(fillMethod, limit))
 
   /** Joins two frames along both their indexes and applies a function to each
     * pair of values; when either value is NA, the result of the function is
@@ -1390,7 +1391,7 @@ class Frame[RX: ST: ORD, CX: ST: ORD, @spec(Int, Long, Double) T](
       m1: ST[O1],
       m2: ST[O2]
   ): Frame[O1, V, T] = {
-    implicit def ordV: ORD[V]  = stkr.ord
+    implicit def ordV: ORD[V] = stkr.ord
     implicit def clmV: ST[V] = stkr.tag
 
     val (lft, rgt) =
@@ -1955,10 +1956,9 @@ object Frame extends BinOpFrame {
   /** Factory method to create a Frame from a sequence of Series. The row labels
     * of the result are the outer join of the indexes of the series provided.
     *
-    * This method repeatedly joins the rows indices. 
-    * Mind the combinatorial semantics of joins, 
-    * if the indices contain duplicates, the resulting Frame can grow quickly.
-    * An alternative is Frame.fromCols.
+    * This method repeatedly joins the rows indices. Mind the combinatorial
+    * semantics of joins, if the indices contain duplicates, the resulting Frame
+    * can grow quickly. An alternative is Frame.fromCols.
     */
   @scala.annotation.nowarn
   def apply[RX: ST: ORD, T: ST: ID](
@@ -2006,10 +2006,9 @@ object Frame extends BinOpFrame {
     * specifying the column index to use. The row labels of the result are the
     * outer join of the indexes of the series provided.
     *
-    * This method repeatedly joins the rows indices. 
-    * Mind the combinatorial semantics of joins, 
-    * if the indices contain duplicates, the resulting Frame can grow quickly.
-    * An alternative is Frame.fromCols.
+    * This method repeatedly joins the rows indices. Mind the combinatorial
+    * semantics of joins, if the indices contain duplicates, the resulting Frame
+    * can grow quickly. An alternative is Frame.fromCols.
     */
   def apply[RX: ST: ORD, CX: ST: ORD, T: ST](
       values: Seq[Series[RX, T]],
@@ -2052,10 +2051,9 @@ object Frame extends BinOpFrame {
     * values. The row labels of the result are the outer join of the indexes of
     * the series provided.
     *
-    * This method repeatedly joins the rows indices. 
-    * Mind the combinatorial semantics of joins, 
-    * if the indices contain duplicates, the resulting Frame can grow quickly.
-    * An alternative is Frame.fromCols.
+    * This method repeatedly joins the rows indices. Mind the combinatorial
+    * semantics of joins, if the indices contain duplicates, the resulting Frame
+    * can grow quickly. An alternative is Frame.fromCols.
     */
   def apply[RX: ST: ORD, CX: ST: ORD, T: ST](
       values: (CX, Series[RX, T])*
